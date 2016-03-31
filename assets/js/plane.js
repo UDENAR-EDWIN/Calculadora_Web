a47137a5c9183942b19e8e9c7245ff49588af8db
EDWIN ORBES <edwin20genius@gmail.com>
2016-03-31 14:56:48 -0500

    Diseño del Plano Cartesiano

---
+++ assets/js/plane.js
@@ -0,0 +1,50 @@
+$(document).on("ready",function(){
+  var plane = document.getElementById('lienzo');
+  var lienzo = plane.getContext('2d');
+  var a =  plane.width;
+  var l = plane.height;
+  var x = 0;
+  var y = 0;
+  var num = -6;
+
+  lienzo.setLineDash([5, 15]);
+  lienzo.lineWidth = 0.8;
+  lienzo.globalAlpha = 0.4;
+  lienzo.font = "20px Georgia";
+
+  lienzo.beginPath();
+  for(var i =0; i<=(a/40); i++){
+    lienzo.moveTo(x,0);
+    lienzo.lineTo(x,l);
+    lienzo.fillText(num+"",x,(l/2)+20);
+    x += 40;
+    num += 1;
+  }
+  lienzo.closePath();
+  lienzo.stroke();
+
+  lienzo.beginPath();
+  for(var i =0; i<=(l/44); i++){
+    lienzo.moveTo(0,y);
+    lienzo.lineTo(a,y);
+    y += 44;
+  }
+  lienzo.closePath();
+  lienzo.stroke();
+
+  lienzo.globalAlpha = 1;
+  lienzo.setLineDash([]);
+  lienzo.lineWidth = 3;
+
+  lienzo.beginPath();
+  lienzo.moveTo(0,l/2);
+  lienzo.lineTo(a,l/2);
+  lienzo.closePath();
+  lienzo.stroke();
+
+  lienzo.beginPath();
+  lienzo.moveTo(a/2,0);
+  lienzo.lineTo(a/2,l);
+  lienzo.closePath();
+  lienzo.stroke();
+})
